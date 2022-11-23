ea862a8765a6ffdd10ca304f3f8662eab378fef0
Carlos Santos <carloscoelho1226@gmail.com>
2022-11-23 10:00:58 -0300

    Special move - En Passant

--- src/application/UI.java
+++ src/application/UI.java
@@ -42,7 +42,7 @@ public class UI {
 		try {
 			String s = sc.nextLine();
 
-			char column = s.charAt(0);
+			char column = s.toLowerCase().charAt(0);
 
 			int row = Integer.parseInt(s.substring(1));
 
